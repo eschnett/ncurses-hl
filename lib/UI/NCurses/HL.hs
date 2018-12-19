84ac1d6ed72ac9ac99791e60d42d352e2d760b7a
Erik Schnetter <schnetter@gmail.com>
2018-12-18 22:19:16 -0500

    Clean up Windows and Panels

--- lib/UI/NCurses/HL.hs
+++ lib/UI/NCurses/HL.hs
@@ -9,6 +9,7 @@ module UI.NCurses.HL
   , runCursesHL
   ) where
 
+import Control.Monad.State
 import Data.List
 import qualified Data.Text as T
 
@@ -66,52 +67,79 @@ data PanelShape = PanelShape Integer Integer Integer Integer
 data DrawState = DrawState { stateWindow :: Window
                            , stateColorID :: ColorID
                            , stateAttributes :: [Attribute]
+                           , stateWindows :: [Window]
+                           , statePanels :: [Panel]
                            }
 
-withWindow :: DrawState -> Update a -> Curses a
-withWindow (DrawState w _ _) = updateWindow w
+type DrawStateM = StateT DrawState Curses
 
-draw :: DrawState -> Drawable -> Curses ()
-draw s (DrawString str) = withWindow s (drawString str)
-draw s (DrawText text) = withWindow s (drawText text)
-draw s (DrawGlyph glyph) = withWindow s (drawGlyph glyph)
-draw s (DrawBorder (BorderGlyphs g1 g2 g3 g4 g5 g6 g7 g8)) =
-  withWindow s (drawBorder g1 g2 g3 g4 g5 g6 g7 g8)
-draw s (DrawBox (BoxGlyphs g1 g2)) = withWindow s (drawBox g1 g2)
-draw s (DrawLineH g y) = withWindow s (drawLineH g y)
-draw s (DrawLineV g x) = withWindow s (drawLineV g x)
-draw s (DrawAt y x d) =
-  do (y0, x0) <- getCursor (stateWindow s)
-     withWindow s (moveCursor y x)
-     draw s d
-     withWindow s (moveCursor y0 x0)
-draw s (DrawWithAttribute a b d) =
-  do withWindow s (setAttribute a b)
-     let as = stateAttributes s
-         as' = if b
-               then union [a] as
-               else delete a as
-     draw s { stateAttributes = as' } d
-     withWindow s (setAttributes (stateAttributes s))
-draw s (DrawWithAttributes as d) =
-  do withWindow s (setAttributes as)
-     draw s { stateAttributes = as } d
-     withWindow s (setAttributes (stateAttributes s))
-draw s (DrawColored c d) =
-  do withWindow s (setColor c)
-     draw s { stateColorID = c } d
-     withWindow s (setColor (stateColorID s))
-draw s (DrawWindow (WindowShape ny nx y x) d) =
-  do w <- newWindow ny nx y x   -- TODO: close window
-     draw s d
-draw s (DrawPanels ps) =
+withWindow' :: DrawState -> Update a -> Curses a
+withWindow' s = updateWindow (stateWindow s)
+
+withWindow :: Update a -> DrawStateM a
+withWindow f =
+  do s <- get
+     lift (withWindow' s f)
+
+draw :: Drawable -> DrawStateM ()
+draw (DrawString str) = withWindow (drawString str)
+draw (DrawText text) = withWindow (drawText text)
+draw (DrawGlyph glyph) = withWindow (drawGlyph glyph)
+draw (DrawBorder (BorderGlyphs g1 g2 g3 g4 g5 g6 g7 g8)) =
+  withWindow (drawBorder g1 g2 g3 g4 g5 g6 g7 g8)
+draw (DrawBox (BoxGlyphs g1 g2)) = withWindow (drawBox g1 g2)
+draw (DrawLineH g y) = withWindow (drawLineH g y)
+draw (DrawLineV g x) = withWindow (drawLineV g x)
+draw (DrawAt y x d) =
+  do w <- stateWindow <$> get
+     (y0, x0) <- lift (getCursor w)
+     withWindow (moveCursor y x)
+     draw d
+     withWindow (moveCursor y0 x0)
+draw (DrawWithAttribute a b d) =
+  do as0 <- stateAttributes <$> get
+     withWindow (setAttribute a b)
+     modify $ \s -> let as = if b
+                          then union [a] as0
+                          else delete a as0
+                    in s { stateAttributes = as }
+     draw d
+     withWindow (setAttributes as0)
+     modify $ \s -> s { stateAttributes = as0 }
+draw (DrawWithAttributes as d) =
+  do as0 <- stateAttributes <$> get
+     withWindow (setAttributes as)
+     modify $ \s -> s { stateAttributes = as }
+     draw d
+     withWindow (setAttributes as0)
+     modify $ \s -> s { stateAttributes = as0 }
+draw (DrawColored c d) =
+  do c0 <- stateColorID <$> get
+     withWindow (setColor c)
+     modify $ \s -> s { stateColorID = c }
+     draw d
+     withWindow (setColor c0)
+     modify $ \s -> s { stateColorID = c0 }
+draw (DrawWindow (WindowShape ny nx y x) d) =
+  do w0 <- stateWindow <$> get
+     w <- lift (newWindow ny nx y x)
+     modify $ \s -> s { stateWindows = w : stateWindows s }
+     modify $ \s -> s { stateWindow = w }
+     draw d
+     modify $ \s -> s { stateWindow = w0 }
+draw (DrawPanels ps) =
   mapM_ drawPanel (reverse ps)
-  where drawPanel :: (PanelShape, Drawable) -> Curses ()
+  where drawPanel :: (PanelShape, Drawable) -> DrawStateM ()
         drawPanel (PanelShape ny nx y x, d) =
-          do w <- newWindow ny nx y x -- TODO: close window
-             p <- newPanel w          -- TODO : delete panel
-             draw s { stateWindow = w } d
-draw w (DrawSome ds) = mapM_ (draw w) ds
+          do w0 <- stateWindow <$> get
+             w <- lift (newWindow ny nx y x)
+             modify $ \s -> s { stateWindows = w : stateWindows s }
+             p <- lift (newPanel w)
+             modify $ \s -> s { statePanels = p : statePanels s }
+             modify $ \s -> s { stateWindow = w }
+             draw d
+             modify $ \s -> s { stateWindow = w0 }
+draw (DrawSome ds) = mapM_ draw ds
 
 
 
@@ -131,9 +159,12 @@ runCursesHL minit rhs app2curses curses2app =
           do DrawConfig d to <- app2curses s
              w <- defaultWindow
              let c = defaultColorID
-             draw (DrawState w c []) d
+             DrawState _ _ _ ws ps <-
+               execStateT (draw d) (DrawState w c [] [] [])
              refreshPanels
              render
+             mapM_ deletePanel ps
+             mapM_ closeWindow ws
              ev <- getEvent w to
              t <- curses2app ev
              rhs t s
